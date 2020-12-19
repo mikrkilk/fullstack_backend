1060743d4603cd5e482c15f034a9f2e6d13579d4
Miikka Kilkkilä <minuns@gmail.com>
2020-12-19 20:59:33 +0200

    backend 3-10

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('json', function (req, res) { 
@@ -76,7 +78,7 @@ app.get('/info', (request, response) => {
     response.send('<div>Phonebook has info for '+persons.length+' people</div><div>'+new Date(Date.now()).toString()+'</div>')
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
