954ab7bd75c87b26e7e918db0a417be7c86d5a7b
Miikka Kilkkilä <minuns@gmail.com>
2020-12-19 20:49:02 +0200

    3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('json', function (req, res) { 
+    return req.method==="POST"?JSON.stringify(req.body):''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+
 
 let persons = [
       {
